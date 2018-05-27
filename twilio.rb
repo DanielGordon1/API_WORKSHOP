b62dd5235a8321d4d2a3fb33ca0150fb5957aa29
Daniel Gordon <d_gordon1@icloud.com>
2018-05-27 16:30:06 +0800

    fix

--- twilio.rb
+++ twilio.rb
@@ -5,11 +5,11 @@ require 'yaml'
 account_sid = YAML.load_file('api_keys.yml')['TWILIO_SID'] # Your Account SID from www.twilio.com/console
 auth_token = YAML.load_file('api_keys.yml')['TWILIO_AUTH'] # Your Auth Token from www.twilio.com/console
 twilio_number = YAML.load_file('api_keys.yml')['TWILIO_NUM']
-my_number = YAML.load_file('api_keys.yml')['MY_NUM']
+my_number = YAML.load_file('api_keys.yml')['MY_NUMBER']
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 message = @client.messages.create(
-    body: "Bla bla bla",
+    body: "Hi guys, this is my program sending your phone a text",
     to: my_number,    # Replace with your phone number
     from: twilio_number)  # Replace with your Twilio number
 
